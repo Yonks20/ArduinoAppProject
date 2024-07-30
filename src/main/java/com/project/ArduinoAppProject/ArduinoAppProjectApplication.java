fbfc758cc3bd9b8b07defbc05d2b1116730ebb67
yonks20 <yonkieyudha@gmail.com>
2024-07-30 19:13:05 +0700

    Add Spring Boot Framework

---
+++ src/main/java/com/project/ArduinoAppProject/ArduinoAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.ArduinoAppProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinoAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArduinoAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ArduinoAppProject/ArduinoAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ArduinoAppProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArduinoAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
